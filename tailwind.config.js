7fa24eacbffcdaf856333c8060224ea18d480591
HellblazerXXL <royceapera@yahoo.com>
2025-05-01 02:04:30 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "360",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
